bef9a1967f5efd98d607830e2dc1d56dbd528e6b
MattWorkHub <150373956+MattWorkHub@users.noreply.github.com>
2024-04-29 01:40:41 +0200

    Add files via upload

---
+++ src/main/java/controllerswagger/ControllerswaggerApplication.java
@@ -0,0 +1,13 @@
+package controllerswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerswaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/controllerswagger/ControllerswaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package controllerswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerswaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
